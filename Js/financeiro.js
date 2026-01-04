e9a78e13a99b1382b0af3216d287e31387181e52
Patrick Lorêdo <phlsjw@gmail.com>
2026-01-04 13:11:36 -0300

    Atualizado Cadastro de Nova Categoria Financeira

--- Js/financeiro.js
+++ Js/financeiro.js
@@ -1,10 +1,6 @@
 const listaContasFinanceiras = JSON.parse(localStorage.getItem("listaContasFinanceiras")) || [];
-localStorage.setItem("listaContasFinanceiras", JSON.stringify(listaContasFinanceiras));
 
-window.onload = function(){
-    geraProximoCodigoContaFinanceira();
-}
-
+/*GERA O CODIGO DA PROXIMA CONTA A SER CADASTRADA*/
 function geraProximoCodigoContaFinanceira () {
     var tamanhoArrayContasFinanceiras = listaContasFinanceiras.length;
     var proximoCodigoContaFinanceira = tamanhoArrayContasFinanceiras + 1;
@@ -12,6 +8,8 @@ function geraProximoCodigoContaFinanceira () {
 
     inputProximoCodigoConta.value = proximoCodigoContaFinanceira;
 }
+
+/*MUDA A COR DO INPUT-GROUP-TEXT CONFORME VALUE DO TIPO DE CONTA*/
 function mudaCorTipoConta(){
     var spanTipoConta = document.getElementById('inputGroupTipoConta');
     var selectTipoConta = document.getElementById('selectTipoConta').value;
@@ -23,4 +21,62 @@ function mudaCorTipoConta(){
         spanTipoConta.classList.remove('bg-success');
         spanTipoConta.classList.add('bg-danger');
     }
+}
+
+/*SALVA A NOVA CONTA FINANCEIRA NO ARRAY*/
+function salvarNovaContaFinanceira(){
+    const codigoNovaContaFinanceira = document.getElementById('inputProximoCodigoConta').value;
+    const tipoNovaContaFinanceira = document.getElementById('selectTipoConta').value;
+    const nomeNovaContaFinanceira = document.getElementById('nomeNovaContaFinanceira').value;
+    const saldoInicialNovaContaFinanceira = document.getElementById('saldoInicialNovaContaFinanceira').value;
+
+    if(
+        tipoNovaContaFinanceira.trim() === '' ||
+        nomeNovaContaFinanceira.trim() === '' ||
+        saldoInicialNovaContaFinanceira.trim() === ''
+    ){
+        alert('Preencha todos os campos antes de Salvar!');
+        return;
+    }
+
+    const contaExiste = listaContasFinanceiras.some(
+        conta => conta.nome === nomeNovaContaFinanceira
+    );
+
+    if(contaExiste){
+        alert('Conta já cadastrada anteriormente!');
+        document.getElementById('nomeNovaContaFinanceira').value = '';
+        return;
+    }
+
+    const novaConta = {
+        codigo: codigoNovaContaFinanceira,
+        tipo: tipoNovaContaFinanceira,
+        nome: nomeNovaContaFinanceira,
+        saldoInicial: saldoInicialNovaContaFinanceira
+    };
+
+    listaContasFinanceiras.push(novaConta);
+
+    // ✅ SALVA NO NAVEGADOR
+    localStorage.setItem(
+        "listaContasFinanceiras",
+        JSON.stringify(listaContasFinanceiras)
+    );
+
+    alert(`Nova conta ${nomeNovaContaFinanceira} salva com sucesso!`);
+
+    limparCadastroNovaContaFinanceira();
+}
+
+/*LIMPA OS CAMPOS DE CADASTRO DE NOVA CONTA FINANCEIRA*/
+function limparCadastroNovaContaFinanceira(){
+    document.getElementById('selectTipoConta').value = 'receita';
+    document.getElementById('nomeNovaContaFinanceira').value = '';
+    document.getElementById('saldoInicialNovaContaFinanceira').value = '';
+    geraProximoCodigoContaFinanceira();
+}
+
+function exibeContaFinanceiraSalva(){
+    
 }
